5f380030629b01a24eba43cdc408d260cad0df91
llhert30 <honeylemonspice123456789@gmail.com>
2026-01-22 19:19:25 -0800

    Added sign up and log in logic + skeleton ui

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,137 @@
-import { Text, View } from "react-native";
+import { supabase } from "@/lib/supabase";
+import { useEffect, useState } from "react";
+import { Alert, StyleSheet, View, TextInput, Button, Text } from 'react-native';
 
-export default function Index() {
+export default function TabOneScreen() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [username, setUsername] = useState('');
+  const [full_name, setFullName] = useState('');
+
+  async function signInWithEmail() {
+    setLoading(true);
+    const { data: authData, error: authError } = await supabase.auth.signInWithPassword({
+      email: email,
+      password: password,
+    });
+
+    if (authError) {
+      console.error(authError);
+      return;
+    }
+
+    // 2. Fetch the profile using the User ID (uid) from the auth step
+    const { data: profileData, error: profileError } = await supabase
+      .from('profiles')
+      .select('*')
+      .eq('id', authData.user.id)
+      .single(); // .single() returns one object instead of an array
+
+    if (profileError) {
+      console.error("Error fetching profile:", profileError);
+    } else {
+      Alert.alert("Profile found! Username: " + profileData.username + ", display name: " + profileData.full_name);
+    }
+    setLoading(false);
+  }
+
+  async function signUpWithEmail() {
+    setLoading(true);
+    const { data: { session }, error } = await supabase.auth.signUp({
+      email: email,
+      password: password,
+      options: {
+        data: {
+          username: username,
+          full_name: full_name,
+        },
+      },
+    });
+
+    if (error) Alert.alert(error.message);
+    if (!session) Alert.alert("Please check your inbox for email verification!");
+    setLoading(false);
+  }
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Studybug Login</Text>
+      
+      <View style={styles.verticallySpaced}>
+        <TextInput
+          onChangeText={(text) => setUsername(text)}
+          value={username}
+          placeholder="Username"
+          style={styles.input}
+        />
+      </View>
+
+      <View style={styles.verticallySpaced}>
+        <TextInput
+          onChangeText={(text) => setFullName(text)}
+          value={full_name}
+          placeholder="Full name"
+          style={styles.input}
+        />
+      </View>
+
+      <View style={styles.verticallySpaced}>
+        <TextInput
+          onChangeText={(text) => setEmail(text)}
+          value={email}
+          placeholder="email@address.com"
+          autoCapitalize={'none'}
+          style={styles.input}
+        />
+      </View>
+      
+      <View style={styles.verticallySpaced}>
+        <TextInput
+          onChangeText={(text) => setPassword(text)}
+          value={password}
+          secureTextEntry={true}
+          placeholder="Password"
+          autoCapitalize={'none'}
+          style={styles.input}
+        />
+      </View>
+
+      <View style={[styles.verticallySpaced, styles.mt20]}>
+        <Button title="Sign in" disabled={loading} onPress={() => signInWithEmail()} />
+      </View>
+      
+      <View style={styles.verticallySpaced}>
+        <Button title="Sign up" disabled={loading} onPress={() => signUpWithEmail()} />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 40,
+    padding: 12,
+  },
+  verticallySpaced: {
+    paddingTop: 4,
+    paddingBottom: 4,
+    alignSelf: 'stretch',
+  },
+  mt20: {
+    marginTop: 20,
+  },
+  input: {
+    height: 50,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 20,
+  }
+});
