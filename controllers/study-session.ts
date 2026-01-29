54475a471794f6bd6b7c48ba823b973255282d63
jqin206 <juneqin2017@gmail.com>
2026-01-28 17:58:45 -0800

    create function to add new entry into study sessions table

---
+++ controllers/study-session.ts
@@ -0,0 +1,24 @@
+import { supabase } from "@/lib/supabase";
+
+export async function startStudySession(startTime: Date, userId: string, isPublic: Boolean, subject: string) {
+    const { data, error } = await supabase
+        .from('study_sessions')
+        .insert([
+            { 
+                user_id: userId, 
+                start_time: startTime.toISOString(),
+                end_time: null,
+                is_public: isPublic, 
+                subject: subject 
+            },
+        ])
+        .select();
+
+    if (error) {
+        console.error('Error inserting data:', error.message);
+        return null;
+    } else {
+        console.log('Successfully added todo:', data);
+        return data;
+    }
+}
