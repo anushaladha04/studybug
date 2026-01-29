4f85b6df31f66b48ef8dfdd5a1569fae3e344037
llhert30 <honeylemonspice123456789@gmail.com>
2026-01-28 17:38:46 -0800

    Add button, no functionality yet

--- app/index.tsx
+++ app/index.tsx
@@ -1,36 +1,43 @@
 import SignOutButton from "@/components/sign-out-button";
 import { useAuthContext } from "@/hooks/use-auth-context";
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, TouchableOpacity, View, Alert } from 'react-native';
 
 export default function HomeScreen() {
-  const { profile } = useAuthContext();
+    const handlePressStart = () => {
+    console.log("button pressed!");
+  };
 
   return (
-    <View> 
-      <View style={styles.container}>
-          <Text style={styles.title}>Welcome!</Text>
-      </View>
-      <View style={styles.container}>
-        <Text>Username</Text>
-        <Text>{profile?.username}</Text>
-        <Text>Full name</Text>
-        <Text>{profile?.full_name}</Text>
-      </View>
-      <SignOutButton />
+    <View style={styles.container}>
+      <TouchableOpacity 
+        style={styles.startButton} 
+        onPress={handlePressStart}
+        activeOpacity={0.7}
+      >
+        <Text style={styles.startIcon}>▶</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 40,
-    padding: 12,
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    marginBottom: 20,
-  }
+  startButton: {
+    width: 120,
+    height: 120,
+    backgroundColor: '#555',
+    borderRadius: 60,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  startIcon: {
+    color: 'white',
+    fontSize: 50,
+    marginLeft: 8,
+  },
 });
