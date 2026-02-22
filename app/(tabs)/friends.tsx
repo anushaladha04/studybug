f91f2ff951f3cb306d90545e82c6197ee1f2d7d1
jqin206 <juneqin2017@gmail.com>
2026-02-21 16:12:51 -0800

    style search bar

--- app/(tabs)/friends.tsx
+++ app/(tabs)/friends.tsx
@@ -1,3 +1,4 @@
+import SearchIcon from '@/assets/images/search.svg';
 import FriendCard from '@/components/friend-card';
 import { acceptFriendRequest, fetchAllFriends, fetchByUsername, fetchFriendRequests, requestFriend } from '@/controllers/friends';
 import { useEffect, useState } from 'react';
@@ -78,13 +79,17 @@ export default function FriendsScreen() {
     <View style={styles.container}>
       <Text style={styles.title}>Friends</Text>
 
-      <TextInput
-        style={styles.searchBar}
-        placeholder="Search friends..."
-        placeholderTextColor="#999"
-        value={searchQuery}
-        onChangeText={setSearchQuery}
-      />
+      <View style={styles.searchBar}>
+        <SearchIcon width={16} height={16} fill='transparent' styles={styles.searchIcon}/>
+        <TextInput
+          style={styles.searchInput}
+          placeholder="Search friends..."
+          placeholderTextColor="#717171"
+          value={searchQuery}
+          onChangeText={setSearchQuery}
+        />
+
+      </View>
 
       { !isSearching && (
         <View style={styles.tabBar}>
@@ -220,15 +225,31 @@ const styles = StyleSheet.create({
     fontFamily: 'monospace',
   },
   searchBar: {
+    flexDirection: 'row',
+    alignItems: 'center',
     width: '90%',
-    height: 42,
+    height: 32,
     backgroundColor: '#f0f0f0',
     borderRadius: 18,
     paddingHorizontal: 7,
     paddingVertical: 5,
-    fontSize: 15,
-    marginTop: 16,
-    color: '#333',
+    gap: 10,
+    shadowColor: '#000000',
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
+    elevation: 4, 
+  },
+  searchIcon: {
+    marginRight: 10,
+  },
+  searchInput: {
+    flex: 1,
+    color: '#717171',
+    fontSize: 14,
   },
   tabBar: {
     flexDirection: 'row',
