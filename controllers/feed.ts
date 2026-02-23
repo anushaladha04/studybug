89541783ce94f1b0f4f6b03d8b1629447ab70a90
jqin206 <juneqin2017@gmail.com>
2026-02-22 20:34:57 -0800

    remove unnecessary controller functions and tests

--- controllers/feed.ts
+++ controllers/feed.ts
@@ -1,27 +1,6 @@
 import { supabase } from "@/lib/supabase";
 import { Float } from "react-native/Libraries/Types/CodegenTypes";
 
-export async function fetchPosts() {
-    const { data: { user } } = await supabase.auth.getUser();
-
-    if (!user) {
-        console.error('No authenticated user found');
-        return [];
-    }
-
-    const { data, error } = await supabase
-        .from('feed_view')
-        .select('*')
-        .eq('user_id', user.id)
-    
-    if (error) {
-        console.error('Error fetching posts:', error.message);
-        return [];
-    }
-
-    return data;
-}
-
 export async function fetchPostsRandomOrder(seed: Float) {
     const { data: { user } } = await supabase.auth.getUser();
 
